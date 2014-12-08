bba49a532fae5692eb79e06b64eb6ce62c48887b
George Dietz <dietzgeorge@gmail.com>
2014-12-07 22:57:23 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+sandbox-py2app-pyqt-rpy2
+========================
+
+Example of using pyqt4, cxFreeze and rpy2 together to make a mac app using R on the
+backend to perform a calculation.
+
+notes:
+	I installed R, python27 and pyqt4 via macports. I installed cxFreeze and rpy2 via pip locally in a virtualenv built from the macport-installed python. The virtualenv was build using the --system-site-packages option so it can see PyQt4 in the macports python installation.  
---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+
+from PyQt4 import QtCore, QtGui
+from PyQt4.Qt import *
+
+import ui_form
+
+print "trying to import rpy2"
+from rpy2 import robjects as ro
+
+print("main started")
+
+class HelloWorldDlg(QtGui.QDialog, ui_form.Ui_Dialog):
+    def __init__(self, parent=None):
+        super(HelloWorldDlg, self).__init__(parent)
+        self.setupUi(self)
+        
+        self.result_lbl.setText("Results is %d" % ro.r("2+3")[0])
+        
+def start():
+    app = QApplication(sys.argv)
+    form = HelloWorldDlg()
+    form.show()
+    form.raise_()
+    app.exec_()
+
+if __name__ == '__main__':
+    #pass        
+    start()
